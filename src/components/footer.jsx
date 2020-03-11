7f0d18beb70558768273ab1ade3426117ab7eed9
uichi <ykonishi@protonmail.com>
2020-03-12 08:34:41 +0900

    FotterのclassをclassNameに変更

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer class="footer">
+    <footer className="footer">
       <small>&copy; Yuichi Konishi</small>
     </footer>
   )
