ac7e1cd1546acc53a86fa6b1d13507d470128e26
uichi <ykonishi@protonmail.com>
2020-03-15 21:19:27 +0900

    Make sekret works page.

--- src/configs/Router.jsx
+++ src/configs/Router.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../components/Home';
 import Profile from '../components/Profile';
 import Works from '../components/Works';
+import SecretWorks from '../components/SecretWorks';
+import NotFoundPage from '../components/NotFoundPage';
 
 const Router = () => {
   return (
@@ -11,6 +13,9 @@ const Router = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/works" component={Works} />
+        <Route exact path="/works/test" component={SecretWorks} />
+        {/* <Route exact path="/works/process.env.REACT_APP_SECRET_WORKS_KEY" component={SecretWorks} /> */}
+        <Route path="*" component={NotFoundPage} />
       </Switch>
     </BrowserRouter>
   );
