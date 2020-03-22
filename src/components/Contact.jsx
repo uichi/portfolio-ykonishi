8b10926037526910503040f24be66aab6a0b83b3
uichi <ykonishi@protonmail.com>
2020-03-22 22:39:58 +0900

    Adjust the contact page.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Helmet from 'react-helmet';
 import Menu from './Menu';
 import Footer from './Footer';
@@ -16,23 +16,25 @@ const Contact = () => {
           <header>
             <h1 className="page-title">Contact</h1>
           </header>
-          <form name="contact" method="post">
+          <form className="contact-form" name="contact" method="post">
             <input type="hidden" name="form-name" value="contact" />
-            <p>
-              <label>お名前: <input type="text" name="name"/></label>
-            </p>
-            <p>
-              <label>Email: <input type="email" name="email"/></label>
-            </p>
-            <p>
-              <label>会社名: <input type="text" name="name"/></label>
-            </p>
-            <p>
-              <label>本文: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-              <button type="submit">送信</button>
-            </p>
+            <label>
+              お名前<span className="required-attention">(必須)</span>
+              <input className="contact-form__input" type="text" name="username" required/>
+            </label>
+            <label>
+              Email<span className="required-attention">(必須)</span>
+              <input className="contact-form__input" type="email" name="email" required/>
+            </label>
+            <label>
+              会社名
+              <input className="contact-form__input" type="text" name="company"/>
+            </label>
+            <label>
+              本文<span className="required-attention">(必須)</span>
+              <textarea className="contact-form__textarea field__textarea" name="message" required></textarea>
+            </label>
+            <button className="contact-form__submit" type="submit">送信</button>
           </form>
         </div>
         <Footer />
